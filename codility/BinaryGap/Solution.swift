88f8d76475e0c160efd703a088950f82af29c4ff
Shota Shimazu <hornet.live.mf@gmail.com>
2020-03-28 00:06:52 +0900

    first commit

---
+++ codility/BinaryGap/Solution.swift
@@ -0,0 +1,24 @@
+import Foundation
+import Glibc
+
+public func solution(_ N : Int) -> Int {    
+    var previousGapCount = 0
+    var currentGapCount = 0
+    
+
+    for char in String(N, radix: 2) {
+        if char == "1" {
+            if currentGapCount > previousGapCount {
+                previousGapCount = currentGapCount
+            }
+            
+            currentGapCount = 0
+        }
+
+        if char == "0" {
+            currentGapCount += 1
+        }
+    }
+    
+    return previousGapCount
+}
